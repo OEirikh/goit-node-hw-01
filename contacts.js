3dab35955ac6971c376d6a0fa0140392f7ecb906
Oleksandr Eirikh <o.eyrikh@gmail.com>
2022-06-07 19:11:13 +0300

    step2

---
+++ contacts.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require("node:path");
+
+const contactsPath = path.normalize("./contacts");
+
+/*
+ * Раскомментируй и запиши значение
+ * const contactsPath = ;
+ */
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
